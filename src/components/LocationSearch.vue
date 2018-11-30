e248cd6417a7f54869c9de2a0cdb287b2171c478
Derek Benak <benak.derek@gmail.com>
2018-11-29 16:30:27 -0800

    updating LocationSearch.vue

--- src/components/LocationSearch.vue
+++ src/components/LocationSearch.vue
@@ -2,8 +2,14 @@
   <div>
     <h2>Location Search</h2>
     <form v-on:submit.prevent="getLocation">
-        <p>Enter location name: <input type="text" v-model="query" placeholder="Seattle, US"> <button type="submit">Go</button></p>
+        <p>Enter a latitude: <input type="text" v-model="lat" placeholder="47.606209"> and longitude: <input type="text" v-model="lng" placeholder="-122.332069"> to find your sunrise and sunset times. <button type="submit">Search</button></p>
     </form>
+      <ul v-if="results && results.length > 0" class="results">
+      <li v-for="item of results" class="item">
+        <p><strong>{{item.word}}</strong></p>
+      </li>
+    </ul>
+    <p>This API is attributed to sunrise-sunset.org located at https://sunrise-sunset.org/api</p>
   </div>
 </template>
 
@@ -37,3 +43,52 @@ export default {
   }
 }
 </script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.rhymesaurus {
+  font-size: 1.4rem;
+}
+input[type="text"]{
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #333;
+  width: 300px;
+  font-size: 1.4rem;
+  color: #2c3e50;
+  font-weight: 300;
+  background: rgba(0,0,0,0.02);
+  padding: 0.5rem;
+}
+button{
+  background: #333;
+  padding: 0.5rem;
+  font-weight: 300;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 1.4rem;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul.results {
+  list-style-type: none;
+  padding: 0;
+}
+.results li {
+  display: inline-block;
+  margin: 10px;
+  border: solid 1px #333;
+  padding: 0.5rem;
+  width: 200px;
+  min-height: 100px;
+  color: #fff;
+  background: rgba(0,0,0,0.7);
+}
+ul.errors {
+  list-style-type: none;
+}
+</style>
